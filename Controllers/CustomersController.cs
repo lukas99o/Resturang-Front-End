283b4527df874633e0e44253f5cb29264b5b3ed0
lukas99o <lukas99o@hotmail.com>
2024-09-13 10:42:52 +0200

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResturangFrontEnd.Models;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ResturangFrontEnd.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly HttpClient _httpClinet;
+        private string baseUrl = "https://localhost:7157/";
+
+        public CustomersController(HttpClient httpClient)
+        {
+            _httpClinet = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Customers";
+
+            var response = await _httpClinet.GetAsync($"{baseUrl}GetAllCustomers");
+            var json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var customerList = JsonSerializer.Deserialize<List<Customer>>(json);
+
+            if (customerList != null)
+            {
+                foreach (var customer in customerList)
+                {
+                    Console.WriteLine(customer.Name);
+                }
+            }
+
+            return View(customerList);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "New Customer";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(customer);
+            }
+
+            var json = JsonSerializer.Serialize(customer);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClinet.PostAsync($"{baseUrl}CreateCustomer", content);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResturangFrontEnd.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        [Required(ErrorMessage = "This field can't be empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field can't be empty")]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
